1e5f602a3995dcfb06fd145c2b264fb98db72cba
nrudro1997 <rudra.newaz@northsouth.edu>
2019-12-08 05:41:08 +0600

    Login File added and profile also added

---
+++ reg_user.php
@@ -0,0 +1,32 @@
+<?php
+	session_start();
+	include_once "connect.php";
+    if(isset($_POST['submit'])){
+
+    	
+
+			$str = "INSERT INTO user (uid, uname, upass, address,  mno ) VALUES
+			('" . $_POST["id"] . "', '" . $_POST["username"] . "', '" . $_POST["password"] ."' , '" . $_POST["address"] . "', '" . ($_POST["mno"]) . "')";
+
+			$result=mysqli_query($con,$str);
+			
+			if(!empty($result)) {
+				$error_message = "";
+				$success_message = "You have registered successfully!";	
+				unset($_POST);
+				header("Location: product.php");
+			} else {
+
+				echo "not done not dones";
+				$error_message = "Problem in registration. Try Again!";	
+			}
+		
+
+	}
+
+		
+?>
+
+
+				
+

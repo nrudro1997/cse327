e66f2517ddb0ae3e77f54745616fdb0f85a37333
nrudro1997 <rudra.newaz@northsouth.edu>
2019-12-08 06:33:56 +0600

    Some files modified

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 	error_reporting (1);
-	$con=mysqli_connect("localhost","root","","bookshop");
+	$con=mysqli_connect("localhost","root","","librarysystem");
 	if ($con==false) {
 		echo "Database Connection is not Successful";
 	}

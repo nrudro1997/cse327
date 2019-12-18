76fb68bc84c4570751fff60c338f2b69585addb5
Shahariarevan <shahariar.hossain01@northsouth.edu>
2019-12-19 02:03:10 +0600

    Deletes connect

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	Error_reporting (1);
-	$con=Mysqli_connect("localhost","root","","librarysystem");
-	if ($con==false)
-   	{
-		echo "Database Connection is not Successful"; // error message
-	}
-?>

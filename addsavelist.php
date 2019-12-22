505c81b5ed5ae1d9243cf4fc46d8a5ca6a6acf0f
nrudro1997 <rudra.newaz@northsouth.edu>
2019-12-23 00:40:08 +0600

    Documentation done in the savelist file

--- addsavelist.php
+++ addsavelist.php
@@ -3,11 +3,11 @@
 
 <?php
 
-			 include 'lib/session.php';
-			 Session::init();
+			 include 'lib/session.php';  //session included
+			 Session::init();  //session initial to start session
 
 			
-			include_once('connect.php');
+			include_once('connect.php');  //Database connection file
 			
 			
 
@@ -22,13 +22,13 @@
 
 	$bid = $_GET['bid'];
 	
-	$sql = "INSERT INTO savelist(uid,bid) VALUES('$id','$bid')";	
+	$sql = "INSERT INTO savelist(uid,bid) VALUES('$id','$bid')";	//Insert book id in the savelist
 
 	if(!mysqli_query($con,$sql))
 			{
 				echo 'course didnot added';
 			}
 	else{
-				header("location:list.php");
+				header("location:list.php"); // After seccessfully added it will redirect to the list page
 			}
 ?>
